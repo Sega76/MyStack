9e51501d44a56632882890f4a0bd195b163e7fe3
sergejryzov <Sega76rus@mail.ru>
2019-03-11 22:22:13 +0300

    first commit

--- src/main/java/MyStack.java
+++ src/main/java/MyStack.java
@@ -1,8 +1,7 @@
 import java.util.Arrays;
-import java.util.Stack;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class MyStack<T> extends Stack<T> {
+public class MyStack<T> {
     private volatile Object[] arr;
     private volatile AtomicInteger next = new AtomicInteger();
     private volatile AtomicInteger size = new AtomicInteger();
@@ -13,7 +12,6 @@ public class MyStack<T> extends Stack<T> {
         size.set(arr.length);
     }
 
-    @Override
     public synchronized T push(T item) {
         if (next.get()==size.get()){
             size.set(size.get()*2);
@@ -24,16 +22,13 @@ public class MyStack<T> extends Stack<T> {
         return (T)arr[next.get()-1];
     }
 
-    @Override
     public synchronized T pop() {
         if(next.get()-1<0){
             return null;
         }
         return (T)arr[next.decrementAndGet()];
-//        return super.pop();
     }
 
-    @Override
     public synchronized T peek() {
         if(next.get()-1<0){
             return null;
@@ -41,13 +36,8 @@ public class MyStack<T> extends Stack<T> {
         return (T)arr[next.get()-1];
     }
 
-    @Override
     public boolean empty() {
         return next.get()==0;
     }
 
-    @Override
-    public synchronized int search(Object o) {
-        return super.search(o);
-    }
 }
